dab0d992fbd58ba25c3c28a99d44e0248deb206e
Altaks <57632793+Altaks@users.noreply.github.com>
2022-05-26 17:43:38 +0200

    project finished

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+from PIL import Image
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# ouvrir l'image
+image = Image.open("image/image.png")
 
+# changer la taille de l'image
+image = image.resize((50, 50))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+ASCII_art_niveaux = [".", ",", "'", ":", ";", "+", "*", "&", "#", "@"]
 
+# préparer le tableau de pixels
+tab = [[None for i in range(image.size[0])] for j in range(image.size[1])]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# récupérer tous les pixels de l'image
+for x in range(image.size[0]):
+    for y in range(image.size[1]):
+        tab[y][x] = image.getpixel((x, y))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# convertir toutes les couleurs en niveau de gris sur 20 niveaux
+for x in range(image.size[0]):
+    for y in range(image.size[1]):
+        tab[y][x] = ASCII_art_niveaux[
+            int(
+                (tab[y][x][0] + tab[y][x][1] + tab[y][x][2]) / 3
+                        * (len(ASCII_art_niveaux) - 1) / 255)
+        ]
+
+# reformer chaque ligne
+result = []
+for ligne in tab:
+    result.append("".join(ligne))
+
+final_image = "\n".join(result)
+
+# écrire dans un fichier
+with open("image/image.txt", "w+") as f:
+    f.write(final_image)
